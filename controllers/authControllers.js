fa865508805c87c3206ed6d94b7782d39cf07e70
ubeyidah <ubeyidah@gamil.com>
2025-05-04 00:06:53 +0300

    implement login api endpoint and fix bug on removing password

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,9 @@
 import User from "../models/userModel.js";
 import { genAndSetToken } from "../utils/genAndSetToken.js";
-import { signupValidation } from "../validations/authValidation.js";
+import {
+  loginValidation,
+  signupValidation,
+} from "../validations/authValidation.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
@@ -32,7 +35,7 @@ export const signup = async (req, res) => {
   }).save();
 
   genAndSetToken(user._id, res);
-  const { password: _, ...userWithoutPassword } = user;
+  const { password: _, ...userWithoutPassword } = user._doc;
 
   res.status(201).json({
     message: "User created successfully",
@@ -41,7 +44,47 @@ export const signup = async (req, res) => {
   });
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const body = req.body;
+
+  const { error, value } = loginValidation.validate(body);
+  if (error) {
+    return res.status(400).json({
+      message: error.details[0].message,
+      data: null,
+      success: false,
+    });
+  }
+
+  const { email, password } = value;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({
+      message: "Email or password is incorrect",
+      data: null,
+      success: false,
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Email or password is incorrect",
+      data: null,
+      success: false,
+    });
+  }
+
+  genAndSetToken(user._id, res);
+  const { password: _, ...userWithoutPassword } = user._doc;
+
+  res.status(200).json({
+    message: "User logged in successfully",
+    data: userWithoutPassword,
+    success: true,
+  });
+};
 
 export const logout = async (req, res) => {};
 
