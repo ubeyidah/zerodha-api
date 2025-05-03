0b92ba84af7b62bd8ce0d609d5728d7d18b3c34e
ubeyidah <ubeyidah@gamil.com>
2025-05-03 23:46:24 +0300

    implement signup api endpoint

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,7 @@
+import User from "../models/userModel.js";
+import { genAndSetToken } from "../utils/genAndSetToken.js";
 import { signupValidation } from "../validations/authValidation.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   const body = req.body;
@@ -9,6 +12,33 @@ export const signup = async (req, res) => {
       .json({ message: error.details[0].message, data: null, success: false });
   }
   const { username, email, password } = value;
+
+  const isExistingUser = await User.findOne({ email });
+  if (isExistingUser) {
+    return res.status(400).json({
+      message: "User already exists",
+      data: null,
+      success: false,
+    });
+  }
+
+  const generateSalt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, generateSalt);
+
+  const user = await new User({
+    username,
+    email,
+    password: hashedPassword,
+  }).save();
+
+  genAndSetToken(user._id, res);
+  const { password: _, ...userWithoutPassword } = user;
+
+  res.status(201).json({
+    message: "User created successfully",
+    data: userWithoutPassword,
+    success: true,
+  });
 };
 
 export const login = async (req, res) => {};
