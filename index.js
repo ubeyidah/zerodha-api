543fbba9d83f63b648d2db4a69b62f43adb15597
ubeyidah <ubeyidah@gamil.com>
2025-05-04 08:58:30 +0300

    add security middlewares rate limiter and helmet

--- index.js
+++ index.js
@@ -6,19 +6,36 @@ import authRoutes from "./routes/authRoutes.js";
 import holdingRoutes from "./routes/holdingRoutes.js";
 import positionRoutes from "./routes/positionRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
-// config
+// config`
 configDotenv();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 const port = process.env.PORT || 3000;
 
+// security
+const limit = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes.",
+});
+app.use(helmet());
+app.use(limit);
+
 // routes
 app.use("/api/v2/auth", authRoutes);
 app.use("/api/v2/holdings", holdingRoutes);
 app.use("/api/v2/positions", positionRoutes);
 app.use("/api/v2/orders", orderRoutes);
+
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to Zerodha API! <a href='https://github.com/ubeyidah/zerodha-api'>See docs </a>",
+  );
+});
 
 //global error handler
 app.use((err, req, res, next) => {
